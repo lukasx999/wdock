6869512a178b22ff0733917d44a4841bc83ac835
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2026-04-25 01:36:17 +0200

    added example for multi threaded execution of windows

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <format>
 #include <print>
+#include <thread>
 
 #include "application.hpp"
 #include "widgets.hpp"
@@ -9,6 +10,37 @@
 #include "config.hpp"
 
 int main() {
+    window a("a", 500, 500);
+    window b("b", 500, 500);
+
+    a.set_anchor(window::anchor::left);
+    b.set_anchor(window::anchor::right);
+
+    a.on_draw([] {
+        glClearColor(1, 0, 0, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+    });
+
+    b.on_draw([] {
+        glClearColor(0, 0, 1, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+    });
+
+    std::jthread ta([&] {
+        a.run();
+    });
+
+    std::jthread tb([&] {
+        b.run();
+    });
+
+    // std::vector<const window*> windows = { &a, &b };
+    // window::run_concurrent(windows);
+
+    return 0;
+}
+
+int main2() {
 
     auto config_path = "config.kdl";
 
