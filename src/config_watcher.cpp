64e4109c0fdc43cdb7c200c9ae790144899ab9e0
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2026-05-10 04:14:48 +0200

    cleaned up config_watcher impl

---
+++ src/config_watcher.cpp
@@ -0,0 +1,87 @@
+#include "config_watcher.hpp"
+
+config_watcher::config_watcher(application& app, std::filesystem::path path)
+: m_app(app)
+, m_config_path(std::move(path))
+{
+
+    m_thread = std::jthread([&](std::stop_token stop_token) {
+        auto stop_fn = std::bind(&std::stop_token::stop_requested, stop_token);
+
+        if (not watch_file(stop_fn))
+            print_error("failed to install watcher for config file at \"{}\"", path.string());
+    });
+
+}
+
+void config_watcher::reload_config() const {
+    std::scoped_lock lock(g_draw_lock);
+
+    try {
+        m_app.load_config(m_config_path);
+        print_info("config was reloaded from \"{}\"", m_config_path.string());
+    } catch (const config_error& error) {
+        print_error("failed to reload config: {}", error.what());
+    }
+
+}
+
+bool config_watcher::watch_file(std::function<bool()> stop_fn) const {
+
+    int fd = inotify_init1(IN_NONBLOCK);
+    if (fd == -1)
+        return false;
+
+    int wd = inotify_add_watch(fd, m_config_path.c_str(), m_inotify_flags);
+    if (wd == -1)
+        return false;
+
+    struct pollfd pfd;
+    pfd.fd = fd;
+    pfd.events = POLLIN;
+
+    while (!stop_fn()) {
+        int num_fds = poll(&pfd, 1, m_poll_timeout_ms);
+        if (num_fds == -1)
+            return false;
+
+        if (num_fds == 1) {
+            if (!(pfd.revents & POLLIN)) continue;
+
+            if (not handle_inotify_event(fd, wd))
+                return false;
+        }
+
+    }
+
+    if (inotify_rm_watch(fd, wd) == -1)
+        return false;
+
+    if (close(fd) == -1)
+        return false;
+
+    return true;
+}
+
+bool config_watcher::handle_inotify_event(int fd, int wd) const {
+
+    struct inotify_event event;
+    ssize_t bytes_read = read(fd, &event, sizeof event);
+    if (bytes_read == 0 || bytes_read == -1)
+        return false;
+
+    // vim will actually swap the edited file with a new file, so
+    // we have to catch that and add the new file back to the watchlist.
+    if (event.mask & IN_IGNORED) {
+        while (true) {
+            wd = inotify_add_watch(fd, m_config_path.c_str(), m_inotify_flags);
+            if (wd != -1) break;
+            if (errno != ENOENT)
+                return false;
+        }
+    }
+
+    reload_config();
+
+    return true;
+}
--- src/config_watcher.hpp
+++ src/config_watcher.hpp
@@ -7,103 +7,22 @@
 #include <poll.h>
 
 #include "application.hpp"
-#include "utils.hpp"
 
 class config_watcher {
     public:
-    config_watcher(application& app, const std::filesystem::path& path)
-    {
-
-        m_thread = std::jthread([&](std::stop_token stop_token) {
-            auto fn = std::bind(reload_config, std::ref(app), path);
-            auto stop_fn = std::bind(&std::stop_token::stop_requested, stop_token);
-
-            if (not watch_file(path, fn, stop_fn))
-                print_error("failed to install watcher for config file at \"{}\"", path.string());
-        });
-
-    }
+    config_watcher(application& app, std::filesystem::path path);
 
     private:
     std::jthread m_thread;
+    application& m_app;
+    const std::filesystem::path m_config_path;
+    const int m_poll_timeout_ms = 50;
 
-    static void reload_config(application& app, const std::filesystem::path& path) {
-        std::scoped_lock lock(g_draw_lock);
+    // vim will only reliably produce IN_MOVE_SELF events, so we have to catch those
+    const int m_inotify_flags = IN_MODIFY | IN_CLOSE_WRITE | IN_MOVE_SELF;
 
-        try {
-            app.load_config(path);
-            print_info("config was reloaded from \"{}\"", path.string());
-        } catch (const config_error& error) {
-            print_error("failed to reload config: {}", error.what());
-        }
-
-    }
-
-    /// @brief calls a function whenever a file is modified.
-    /// @param fn the function to be called when the file is modified
-    /// @param stop_fn stops the watching process and returns if this function returns true
-    /// @return returns false if the watcher could not be installed
-    static bool watch_file(const std::filesystem::path& path, std::function<void()> fn, std::function<bool()> stop_fn) {
-
-        // vim will only reliably produce IN_MOVE_SELF events, so we have to catch those
-        auto inotify_flags = IN_MODIFY | IN_CLOSE_WRITE | IN_MOVE_SELF;
-
-        int fd = inotify_init1(IN_NONBLOCK);
-        if (fd == -1)
-            return false;
-
-        int wd = inotify_add_watch(fd, path.c_str(), inotify_flags);
-        if (wd == -1)
-            return false;
-
-        struct pollfd pfd;
-        pfd.fd = fd;
-        pfd.events = POLLIN;
-
-        while (!stop_fn()) {
-            int num_fds = poll(&pfd, 1, 100);
-            if (num_fds == -1)
-                return false;
-
-            if (num_fds == 1) {
-                if (!(pfd.revents & POLLIN)) continue;
-
-                if (not handle_inotify_event(fd, wd, path, inotify_flags, fn))
-                    return false;
-            }
-
-        }
-
-        if (inotify_rm_watch(fd, wd) == -1)
-            return false;
-
-        if (close(fd) == -1)
-            return false;
-
-        return true;
-    }
-
-    static bool handle_inotify_event(int fd, int wd, const std::filesystem::path& path, int flags, std::function<void()> fn) {
-
-        struct inotify_event event;
-        ssize_t bytes_read = read(fd, &event, sizeof event);
-        if (bytes_read == 0 || bytes_read == -1)
-            return false;
-
-        // vim will actually swap the edited file with a new file, so
-        // we have to catch that and add the file back to the watchlist
-        if (event.mask & IN_IGNORED) {
-            while (true) {
-                wd = inotify_add_watch(fd, path.c_str(), flags);
-                if (wd != -1) break;
-                if (errno != ENOENT)
-                    return false;
-            }
-        }
-
-        fn();
-
-        return true;
-    }
+    void reload_config() const;
+    bool watch_file(std::function<bool()> stop_fn) const;
+    bool handle_inotify_event(int fd, int wd) const;
 
 };
